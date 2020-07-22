7df390b74930370c0ab00509fb6d1416a55b9a15
Robert McGrew <robbie2497@gmail.com>
2020-07-22 06:59:29 -0600

    updating homework

---
+++ routes/apiRoutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const notesData = require("../db/db.json");
+
+
+
+module.exports = function(app) {
+
+
+  app.get("/api/notes", function(req, res) {
+    res.json(notesData);
+    console.log("GET Works")
+  });
+
+  app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    notesData.push(newNote);
+    res.json(newNote);
+    console.log(req);
+  });
+
+};
